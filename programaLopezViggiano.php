eadbe9e127a7e03cb79aa9ed0273dd825d90a0c4
LopezAlejo0 <alejo.lopez@est.fi.uncoma.edu.ar>
2024-11-18 20:59:46 -0300

    Implementación de función seleccionarOpción

--- programaLopezViggiano.php
+++ programaLopezViggiano.php
@@ -11,9 +11,40 @@ include_once("wordix.php");
 /* ****COMPLETAR***** */
 
 
-/**************************************/
-/***** DEFINICION DE FUNCIONES ********/
-/**************************************/
+/**************************************************/
+/***** DEFINICIÓN DE FUNCIONES PRINCIPALES ********/
+/**************************************************/
+
+/**
+ * Explicación 3 (punto 3)
+ * Muestra por pantalla un menú con las opciones disponibles para el jugador
+ * @return int
+ */
+function seleccionarOpción () {
+    // int $opción
+    do {
+        echo "1) Jugar con una palabra determinada \n";
+        echo "2) Jugar con una palabra aleatoria \n";
+        echo "3) Mostrar una partida \n";
+        echo "4) Mostrar la primer partida ganadora \n";
+        echo "5) Mostrar estadísticas \n";
+        echo "6) Mostrar listado de partidas ordenadas por jugador y por palabras \n";
+        echo "7) Agregar una nueva palabra de 5 letras \n";
+        echo "8) Salir \n";
+        echo "Seleccione una opción: ";
+        $opción = trim (fgets (STDIN));
+        if (!($opción >= 1 && $opción <= 8)) {
+            echo "La opción no es válida, intente nuevamente"; // Cartel de error
+        }
+    } while (!($opción >= 1 && $opción <= 8)); // Si la opción se sale del rango, repetir el menú y volver a solicitar
+    return $opción;
+}
+
+
+
+
+
+
 
 /**
  * Obtiene una colección de palabras
